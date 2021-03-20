968f966d5f7ac88ee09e3686d3611720a0897967
Vladislav Kibenko <wolfram.deus@gmail.com>
2021-03-20 22:26:44 +0500

    feat(mongo): add db variables and di client token

--- src/shared/di/tokens.ts
+++ src/shared/di/tokens.ts
@@ -1,8 +1,14 @@
 import {Token} from 'typedi';
 import {IConfig} from '~/shared/config';
+import {MongoClient} from 'mongodb';
 
 /**
  * Application config.
  * @type {Token<IConfig>}
  */
 export const ConfigToken = new Token<IConfig>();
+
+/**
+ * Connected MongoDB client.
+ */
+export const MongoClientToken = new Token<MongoClient>();
--- src/shared/di/utils.ts
+++ src/shared/di/utils.ts
@@ -1,12 +1,33 @@
 import {Container} from 'typedi';
 import {config} from '~/shared/config';
-import {ConfigToken} from './tokens';
+import {ConfigToken, MongoClientToken} from './tokens';
+import {MongoClient} from 'mongodb';
 
+/**
+ * Inject synchronous dependencies.
+ */
+function injectSyncDependencies() {
+  Container.set(ConfigToken, config);
+}
+
+/**
+ * Injects asynchronous dependencies.
+ */
+async function injectAsyncDependencies() {
+  const {dbPort, dbHost} = config;
+  const mongoDBClient = await new MongoClient(`mongodb://${dbHost}:${dbPort}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).connect();
+
+  Container.set(MongoClientToken, mongoDBClient);
+}
 /**
  * Injects dependencies. It is important to launch project with this function
  * due to there can be dependencies which are injected asynchronously.
  * @returns {Promise<void>}
  */
 export async function injectDependencies() {
-  Container.set(ConfigToken, config);
+  injectSyncDependencies();
+  await injectAsyncDependencies();
 }
